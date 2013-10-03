c415e3bc9b833dda274942fc5a01798bfa30d938
Jose Garmilla <garmilla@astro.princeton.edu>
2013-10-03 13:58:37 -0400

    Update runner.

--- code/current/run_classifier.py
+++ code/current/run_classifier.py
@@ -2,18 +2,18 @@ import numpy as np
 from HBsep import HBsep
 import pdb
 
-Nzs = [1, 10, 10]
-z_maxs = [0.0, 4.0, 7.0]
+Nzs = [1, 30, 30]
+z_maxs = [0.0, 1.5, 7.0]
 class_labels = ['star', 'galaxy', 'qso']
 filter_list_path = 'filter_list_path'
 list_of_sed_list_paths = ['sed/star', 'sed/galaxy', 'sed/qso']
 data = 'result.txt'
 method = 1
 filename = 'classification.fit'
-z_median = [0.0, 2.0, 3.5]
+z_median = [0.0, 1.0, 2.0]
 z_pow = [0.0, 0.0, 0.0]
 
-pdb.set_trace()
+#pdb.set_trace()
 classifier = HBsep(class_labels, Nzs, z_maxs)
 classifier.get_filter_norm(filter_list_path)
 classifier.create_models(filter_list_path, list_of_sed_list_paths)
