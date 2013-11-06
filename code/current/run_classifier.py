77fb3114eb1b10b8bbd8554290ff961a3ecafe5f
Jose Garmilla <garmilla@astro.princeton.edu>
2013-11-06 09:49:43 -0500

    Pickle the classifier object after run is finished.

--- code/current/run_classifier.py
+++ code/current/run_classifier.py
@@ -1,17 +1,18 @@
 import numpy as np
 from HBsep import HBsep
 import pdb
+from datetime import datetime
+import pickle
 
 Nzs = [1, 30, 30]
-z_maxs = [0.0, 1.5, 7.0]
+z_maxs = [0.0, 2.0, 5.0]
 class_labels = ['star', 'galaxy', 'qso']
 filter_list_path = 'filter_list_path'
 list_of_sed_list_paths = ['sed/star', 'sed/galaxy', 'sed/qso']
-data = 'result.txt'
+data = 'result_patch.txt'
 method = 1
-filename = 'classification.fit'
-z_median = [0.0, 1.0, 2.0]
-z_pow = [0.0, 0.0, 0.0]
+z_median = [0.0, 0.3, 1.0]
+z_pow = [0.0, 2.5, 2.5]
 
 #pdb.set_trace()
 classifier = HBsep(class_labels, Nzs, z_maxs)
@@ -20,5 +21,10 @@ classifier.create_models(filter_list_path, list_of_sed_list_paths)
 classifier.read_and_process_data(data)
 classifier.fit_models()
 classifier.coefficient_marginalization()
-classifier.optimize(z_median, z_pow)
-classifier.write_relative_likelihoods(filename)
+func, xopt = classifier.optimize(z_median, z_pow)
+dt = datetime.now()
+fname = 'classifications/{0}.fit'.format(dt.strftime('%y%m%d%H%M'))
+classifier.write_relative_likelihoods(fname)
+fname = 'classifications/{0}.pkl'.format(dt.strftime('%y%m%d%H%M'))
+with open(fname, 'w') as f:
+    pickle.dump(classifier, f)
